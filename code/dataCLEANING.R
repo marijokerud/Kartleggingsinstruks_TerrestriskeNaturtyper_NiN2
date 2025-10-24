4caf61fea32ea8ed15a81fe556f537b0196ce159
marijokerud <mari.jokerud@gmail.com>
2025-10-24 15:31:47 +0200

    change Naturmangfold and Tilstand to naturmangfold1 and tilstand1

--- code/dataCLEANING.R
+++ code/dataCLEANING.R
@@ -1,3 +1,6 @@
+library(readxl)     # if you load from xlsx
+library(openxlsx)   # for write.xlsx if needed
+
 ###### Found out that nature type C01_Hule eiker was classified as Semi-naturligMark in 2018, changing to Skog.
 ###### And change Høstingsskog (C02) to semi-naturlig
 data_clean1 <- dat2_long_4 %>%
@@ -13,8 +16,8 @@ data_clean1 <- dat2_long_4 %>%
   )
 
 data_clean2 <- data_clean1 %>% 
-  mutate(Naturmangfold= as.numeric(substr(naturmangfold, 1, 1))) %>% 
-  mutate(Tilstand= as.numeric(substr(tilstand, 1, 1))) %>% 
+  mutate(naturmangfold1 = as.numeric(substr(naturmangfold, 1, 1))) %>% 
+  mutate(tilstand1 = as.numeric(substr(tilstand, 1, 1))) %>% 
   #select(-NiN_variable_code, -NiN_variable_value) %>% 
   distinct()
 data_clean2
@@ -23,7 +26,7 @@ data_clean2
 #identify duplicates
 dup.naturtype <- data_clean2 |>
   dplyr::summarise(n = dplyr::n(), .by = c(identifikasjon_lokalId, hovedokosystem, kartleggingsar, lokalitetskvalitet, mosaikk, naturmangfold, naturtype,
-                                           naturtypekode_short, naturtype_full, tilstand, region, km2, m2, maned, oppdragstaker, Naturmangfold, Tilstand, 
+                                           naturtypekode_short, naturtype_full, tilstand, region, km2, m2, maned, oppdragstaker, naturmangfold1, tilstand1, 
                                            NiN_variable_code)) |>
   dplyr::filter(n > 1L)
 
@@ -39,7 +42,7 @@ n_removed
 
 dup2.naturtype <- data_clean3 |>
   dplyr::summarise(n = dplyr::n(), .by = c(identifikasjon_lokalId, hovedokosystem, kartleggingsar, lokalitetskvalitet, mosaikk, naturmangfold, naturtype,
-                                           naturtypekode_short, naturtype_full, tilstand, region, km2, m2, maned, oppdragstaker, Naturmangfold, Tilstand, 
+                                           naturtypekode_short, naturtype_full, tilstand, region, km2, m2, maned, oppdragstaker, naturmangfold1, tilstand1, 
                                            NiN_variable_code)) |>
   dplyr::filter(n > 1L)  
 
@@ -64,5 +67,14 @@ data_clean5 <- data_clean4 %>%
   filter(NiN_variable_code != "4DG-0" |
            naturtypekode_short == "C20") %>% 
   filter(NiN_variable_code != "4DL-0" |
-           naturtypekode_short %in% c("C20", "C21", "E11_05") )
+           naturtypekode_short %in% c("C20", "C21", "E11_05") ) %>% 
+  filter(NiN_variable_code != "7SD-0" |
+           naturtypekode_short %in% c("C11_01", "C12_01", "C13", "C14", "C21", "C22", "E11_01") ) %>% 
+  filter(NiN_variable_code != "7SD-NS" |
+           naturtypekode_short %in% c("C10", "C11_01", "C11_02", "C11_03", "C11_04", "C11_05", "C12_01", "C12_02", "C12_03", "C12_04", "C12_05", "C13", "C14", "C22", "E11_01") )
+
+#drop rows where 
+data_clean6 <- data_clean5 %>%
+  filter(!tilstand1 == "0" )
+
 
